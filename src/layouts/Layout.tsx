eee813679f3e04636773329dbb43340cde27510e
Parth Vaswani <parthnvaswani@gmail.com>
2024-03-25 16:55:26 +0530

    Fix: tsc lint fix

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,11 @@
 import Header from "./Header";
 import Footer from "./Footer";
 
-export default function Layout({ children }) {
+interface LayoutProps {
+	children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
 	return (
 		<>
 			<Header />
